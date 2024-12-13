6b0622a3cd2dfd7eb54f087c4e6e213dc2e42c5f
Tejac12 <chintalatejavardhan692@gmail.com>
2024-12-13 11:09:35 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,47 @@
+// var k = document.getElementById("p");
+// k.style.backgroundColor = "red"
+// k.style.fontSize = "40px"
+// console.log(k.textContent)
+// k.textContent = "hi how are you boy"
+// k.innerHTML = `hi how are <span style="background-color:yellow;font-size:20px">you</span> boy <a href="">youtube</a> `
+// console.log(k)
+
+
+// let k = document.getElementById("p")
+// let a = document.getElementById("btn");
+// function b() {
+//     k.textContent = "Im p but clicked by button"
+//     k.style.backgroundColor = "red"
+//     k.style.fontSize = "60px"
+//     k.innerHTML = `Im p but clicked by button <img src="https://gratisography.com/wp-content/uploads/2024/10/gratisography-cool-cat-800x525.jpg " width="100"/>`
+// }
+
+
+
+
+
+var u=document.getElementById("btn");
+
+
+var k=document.getElementById("p");
+
+function a(){
+    k.style.backgroundColor="red"
+k.style.fontSize="60px"
+k.style.borderRadius="50px"
+
+}
+
+var v=document.getElementById("btn1");
+v.id="image"
+
+function b(){
+    v.style.display="flex"
+    v.style.gap="10px"
+    v.innerHTML = `<img src="https://www.shutterstock.com/image-photo/calm-weather-on-sea-ocean-260nw-2212935531.jpg" width="350" id="img"/> 
+    <img src="https://th.bing.com/th/id/OIP.HxV79tFMPfBAIo0BBF-sOgHaEy?w=299&h=192&c=7&r=0&o=5&dpr=1.3&pid=1.7" width="350" id="img1"/>
+    <img src="https://th.bing.com/th/id/OIP.6Axh0-PYY8UQVpfS08w3gwHaE8?w=288&h=192&c=7&r=0&o=5&dpr=1.3&pid=1.7" width="350" id="img2"/>`
+    
+}
+
+console.log(v);
